2e2a3a95b7d5579686de14c92dbc665008a3570a
Marcos Paulo Soares <marcos.paulo.s.m.filho@gmail.com>
2024-12-09 14:45:44 -0300

    Feat: geração de documentação com springdoc

--- to-do/src/main/java/com/aprendizado/to_do/Config/SecurityConfig.java
+++ to-do/src/main/java/com/aprendizado/to_do/Config/SecurityConfig.java
@@ -39,11 +39,14 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/users").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .anyRequest().authenticated()
             )
             .csrf(csrf -> csrf.disable())
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+            http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**");
         return http.build();
     }
 
